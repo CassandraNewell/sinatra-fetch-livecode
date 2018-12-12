b459d0f76ff4f532dfc07ffb12369aa4cb648db7
Cassandra Newell <cassandra.newell@protonmail.com>
2018-12-12 17:01:10 -0500

    final code

--- public/main.js
+++ public/main.js
@@ -1,17 +1,59 @@
 let appendLocationsArrayToHtml = (locations) => {
   let locationList = document.getElementById('locations')
+
   locations.forEach(location => {
     locationList.innerHTML += `<li>${location.city}, ${location.country}</li>`
   })
 }
 
 let fetchLocations = () => {
-
+  fetch('/api/v1/locations.json')
+  .then(response => {
+    if(response.ok) {
+      return response
+    } else {
+      let error = new Error('Something went wrong in GET')
+      throw(error)
+    }
+  })
+  .then(response => response.json())
+  .then(body => {
+    appendLocationsArrayToHtml(body.locations)
+  })
 }
 
 let postLocation = (event) => {
   event.preventDefault()
 
+  let cityInputField = document.getElementById('city')
+  let countryInputField = document.getElementById('country')
+
+  let newLocation = {
+    location: {
+      city: cityInputField.value,
+      country: countryInputField.value
+    }
+  }
+
+  fetch('/api/v1/locations.json', {
+    method: 'POST',
+    body: JSON.stringify(newLocation)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response
+    } else {
+      let error = new Error('Error in fetch POST')
+      throw(error)
+    }
+  })
+  .then(response => response.json())
+  .then(body => {
+    appendLocationsArrayToHtml([body.location])
+  })
+  .catch(error => {
+    document.getElementById('error').innerHTML += error.message
+  })
 }
 
 // run automatically on page load
--- server.rb
+++ server.rb
@@ -6,9 +6,9 @@ set :bind, '0.0.0.0'
 
 def write_to_json_file(location)
   file = File.read("locations.json")
-  locations_array = JSON.parse(file)
+  locations_hash = JSON.parse(file)
 
-  new_location_id = locations_array["locations"].last["id"] + 1
+  new_location_id = locations_hash["locations"].last["id"] + 1
   new_location = {
     id: new_location_id,
     city: location["city"],
@@ -16,7 +16,7 @@ def write_to_json_file(location)
   }
 
   updated_locations = {
-    locations: locations_array["locations"].concat([new_location])
+    locations: locations_hash["locations"].concat([new_location])
   }
 
   updated_locations_json = JSON.pretty_generate(updated_locations, indent: ' ')
@@ -29,4 +29,23 @@ end
 
 get "/locations" do
   File.read('public/index.html')
+end
+
+get '/api/v1/locations.json' do
+  status 200
+  content_type :json
+  File.read('locations.json')
+end
+
+post '/api/v1/locations.json' do
+  request_json = JSON.parse(request.body.read)
+
+  if request_json["location"]
+    write_to_json_file(request_json["location"])
+    status 200
+    request_json.to_json
+  else
+    status 500
+    {error: "Oops, your data were weird and I hate it"}
+  end
 end
