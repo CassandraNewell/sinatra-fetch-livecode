9e29a681ca2fc141700e289a34554dec33dfb0bf
Cassandra Newell <cassandra.newell@protonmail.com>
2019-03-20 14:50:18 -0400

    Add helper comments to README; tweaks

--- public/main.js
+++ public/main.js
@@ -1,21 +1,21 @@
 let appendLocationsArrayToHtml = (locations) => {
   let locationList = document.getElementById('locations')
+
   locations.forEach(location => {
     locationList.innerHTML += `<li>${location.city}, ${location.country}</li>`
   })
 }
 
 let fetchLocations = () => {
-
+  # Your code here
 }
 
 let postLocation = (event) => {
-  event.preventDefault()
-
+  # Your code here
 }
 
-// run automatically on page load
 fetchLocations()
+
 document
   .getElementById('new-location-submit-button')
   .addEventListener('click', postLocation)
